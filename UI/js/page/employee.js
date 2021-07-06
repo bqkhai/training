4fbbfa672f9f346505d59ec8677de39c68ca09c3
bqkhai00x <56033160+bqkhai00x@users.noreply.github.com>
2021-07-06 17:30:32 +0700

    ..

--- UI/js/page/employee.js
+++ UI/js/page/employee.js
@@ -9,27 +9,41 @@ $(document).ready(function () {
  * Author: BQKhai (05/07/2021)
  */
 function initEvents() {
-    $('table #tbListDAta tbody').on('click', 'tr', function () {
-        $(this).css('background', '#8ec252');
+    var me = this;
+
+    //Sự kiện highlight khi click chuột trên bảng
+    // $('#tbListData tbody').on('click', 'tr', function () {
+    //     $('.m-dialog').css('display', 'block');
+    //     $(this).css('background', '#8ec252');
+    // })
+
+    $("#tbListData tbody").on('click', 'tr', function (){
+        $('tr').removeClass('selected');
+        $(this).addClass('selected');
     })
 
-    $('#btnAdd').click(function () {
-        alert("hello");
+    $("#tbListData tbody").on('dblclick', 'tr', function (){
+        $('tr').removeClass('selected');
+        $(this).addClass('selected');
+        $('.m-dialog').css('display', 'block');
+    })
+
+    // Sự kiện click button refresh
+    $("#my-btn").click(function (){
+        me.loadData();
     })
 }
 
 
 /**
- * Load du lieu danh sach nhan vien
+ * Load dữ liêu danh sách nhân viên
  * Author: BQKhai (05/07/2021)
  * 
  */
-
 function loadData() {
-    //1. Lay du lieu tu API ve:
-
+    //1. Lấy dữ liệu từ API về
     $.ajax({
-        method: "GET", //GET: Lay du lieu; POST: Thêm mới; PUT: sửa; DELETE: xóa.
+        method: "GET", //GET: Lấy dữ liệu; POST: Thêm mới; PUT: sửa; DELETE: xóa.
         url: "http://cukcuk.manhnv.net/v1/Employees",
         // data:"",
         // contentType:"application/json",
@@ -42,29 +56,33 @@ function loadData() {
             let employeeCode = item.EmployeeCode;
             let fullName = item.FullName;
             let genderName = item.GenderName;
-            if (genderName == null) {
+            if (genderName === null) {
                 genderName = 'Không xác định'
             }
             let dateOfBirth = item.DateOfBirth;
-            let dateOrigin = new Date(dateOfBirth);
+            dateOfBirth = formatDate(dateOfBirth);
             let phone = item.PhoneNumber;
             let mail = item.Email;
-            let postName = item.PositionName;
+            let address = item.IdentityPlace;
             let departmentName = item.DepartmentName;
             let salary = item.Salary;
+            salary = formatSalary(salary);
             let workStatus = item.WorkStatus;
+            if (workStatus === null) {
+                workStatus = 'Không xác định'
+            }
             let trHtml = `
                 <tr>
                     <td>${employeeCode}</td>
                     <td>${fullName}</td>
                     <td>${genderName}</td>
-                    <td>${dateOrigin}</td>
+                    <td>${dateOfBirth}</td>
                     <td>${phone}</td>
                     <td>${mail}</td>
-                    <td>${postName}</td>
+                    <td>${address}</td>
                     <td>${departmentName}</td>
-                    <td>${salary}</td>
-                    <td style="max-width: 100px;">${workStatus}</td>
+                    <td style="text-align:right;">${salary}</td>
+                    <td>${workStatus}</td>
                 </tr>`;
             $('tbody').append(trHtml);
         })
@@ -74,7 +92,37 @@ function loadData() {
         alert("Có lỗi xảy ra!");
     })
 
-    //2. Xu ly du lieu:
+    //2. Xử lý dữ liệu:
+    
+    //3. Hiển thị dữ liệu;
+}
 
-    //3. Hien thi du lieu :
+/**
+ * Format dữ liệu ngày tháng năm dạng json sang ngày / tháng / năm
+ * @param {*} date tham số có kiểu dữ liệu bắt kỳ
+ * @returns 
+ * CreatedBy: bqkhai (6/7/2021)
+ */
+function formatDate(date){
+    var date = new Date(date);
+    if(Number.isNaN(date.getTime())){
+        return "";
+    }
+    else {
+        var day = date.getDate(),
+            month = date.getMonth() + 1;
+            year = date.getFullYear();
+        day = day < 10 ? '0' + day : day;
+        month = month < 10 ? '0' + month : month;
+    }
+    return day + '/' + month + '/' + year;
+}
+
+/**
+ * hàm định dạng tiền lương
+ * @param {*} salary Số tiền
+ * Created by bqkhai (6/7/2021)
+ */
+function formatSalary(money){
+    return money.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
