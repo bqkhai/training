646fffdd0f581f434cc00b1897f5ddef090d7f62
bqkhai00x <56033160+bqkhai00x@users.noreply.github.com>
2021-07-06 10:22:04 +0700

    ..

--- UI/js/page/employee.js
+++ UI/js/page/employee.js
@@ -1,35 +1,80 @@
 $(document).ready(function () {
-    let Data = Array.from({ length: 50 }, () => {
-        return (
-            `<tr>
-                <td>MF921</td>
-                <td>Bùi Quang Khải</td>
-                <td>Nam</td>
-                <td>28/07/2000</td>
-                <td>0978308894</td>
-                <td>18020671@vnu.edu.vn</td>
-                <td>Fresher</td>
-                <td>Sản xuất</td>
-                <td>999999</td>
-                <td style="max-width: 100px;">Đang làm</td>
-            </tr>`
-        )
-    });
+    loadData();
+    initEvents();
+})
 
-    $("tbody").html(Data);
-});
+/**
+ * 
+ * Khởi tạo sự kiện
+ * Author: BQKhai (05/07/2021)
+ */
+function initEvents() {
+    $('table #tbListDAta tbody').on('click', 'tr', function () {
+        $(this).css('background', '#8ec252');
+    })
 
-// Format dữ liệu ngày tháng sang ngày/tháng/năm
-// function formatDate(date) {
-//     var date = new Date(date);
-//     if (Number.isNaN(date.getTime())) {
-//         return "";
-//     } else {
-//         let day = date.getDate()
-//         let month = date.getMonth() + 1;
-//         let year = date.getFullYear();
-//         day = day < 10 ? '0' + day : day;
-//         month = month < 10 ? '0' + month : month;
-//         return `${day}/${month}/${year}`;
-//     }
-// }
+    $('#btnAdd').click(function () {
+        alert("hello");
+    })
+}
+
+
+/**
+ * Load du lieu danh sach nhan vien
+ * Author: BQKhai (05/07/2021)
+ * 
+ */
+
+function loadData() {
+    //1. Lay du lieu tu API ve:
+
+    $.ajax({
+        method: "GET", //GET: Lay du lieu; POST: Thêm mới; PUT: sửa; DELETE: xóa.
+        url: "http://cukcuk.manhnv.net/v1/Employees",
+        // data:"",
+        // contentType:"application/json",
+        // dataType:"json"
+    }).done(function (response) {
+        let data = response;
+
+        //Duyệt từng đối tượng xử lý các thông tin
+        $.each(data, function (index, item) {
+            let employeeCode = item.EmployeeCode;
+            let fullName = item.FullName;
+            let genderName = item.GenderName;
+            if (genderName == null) {
+                genderName = 'Không xác định'
+            }
+            let dateOfBirth = item.DateOfBirth;
+            let dateOrigin = new Date(dateOfBirth);
+            let phone = item.PhoneNumber;
+            let mail = item.Email;
+            let postName = item.PositionName;
+            let departmentName = item.DepartmentName;
+            let salary = item.Salary;
+            let workStatus = item.WorkStatus;
+            let trHtml = `
+                <tr>
+                    <td>${employeeCode}</td>
+                    <td>${fullName}</td>
+                    <td>${genderName}</td>
+                    <td>${dateOrigin}</td>
+                    <td>${phone}</td>
+                    <td>${mail}</td>
+                    <td>${postName}</td>
+                    <td>${departmentName}</td>
+                    <td>${salary}</td>
+                    <td style="max-width: 100px;">${workStatus}</td>
+                </tr>`;
+            $('tbody').append(trHtml);
+        })
+        console.log(response);
+
+    }).fail(function (response) {
+        alert("Có lỗi xảy ra!");
+    })
+
+    //2. Xu ly du lieu:
+
+    //3. Hien thi du lieu :
+}
